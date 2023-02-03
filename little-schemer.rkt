0c4442fe4b8aa3c30707b9059400c596df8400b6
Phil Crissman <phcrissm@microsoft.com>
2023-02-03 07:23:18 -0800

    Small changes

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -1,13 +1,5 @@
 #lang racket
-(require rackunit)
-(require racket/string)
 
-(define assert-that
-  (lambda (f a b message)
-    (cond
-      ((f a b) (print message))
-      (else (print (string-append "It was not true that" message "\n"))))))
-    
 
 ; Chapter 1 - Toys
 
@@ -22,9 +14,11 @@
 ; -> (car '()) ; not allowed
 
 ; the car of '(a b) is 'a
-(assert-that eq? 'a (car '(a b)) "'a is equal to (car '(a b))")
+(eq? 'a (car '(a b)))
 
-(assert-that eq? '(b) (cdr '(a b)) "'(b) is eq to (cdr '(a b))")
+; the cdr 
+
+
 
 ; Chapter 2 - Do it, do it again, and again, and again...
 
