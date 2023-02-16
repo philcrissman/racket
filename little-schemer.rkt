f038c85afce424391af5824cc6c316a56ccfe22f
Phil Crissman <phil.crissman@gmail.com>
2023-02-16 09:32:27 -0800

    Added insertR*

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -712,3 +712,22 @@
 (check-equal?
   (rember* 'sauce '(((tomato sauce)) ((bean) sauce) (and ((flying)) sauce)))
   '(((tomato)) ((bean)) (and ((flying)))))
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+              (cond
+                ((eq? (car l) old) (cons (car l) (cons new (insertR* new old (cdr l)))))
+                (else (cons (car l) (insertR* new old (cdr l))))))
+      (else (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
+
+(check-equal?
+ (insertR* 'ho 'hey '(((hey) lets) (go) (hey (lets (go)))))
+ '(((hey ho) lets) (go) (hey ho (lets (go)))))
+
+(check-equal?
+ (insertR* 'roast 'chuck '((how much (wood)) could ((a (wood) chuck)) (((chuck))) (if (a) ((wood chuck))) could chuck wood))
+ '((how much (wood)) could ((a (wood) chuck roast)) (((chuck roast))) (if (a) ((wood chuck roast))) could chuck roast wood))
+
